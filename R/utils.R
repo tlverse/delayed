b212d2ffe837f9a8e95982999d328a5ed7b6432c
Jeremy Coyle <jeremyrcoyle@gmail.com>
2017-10-03 11:51:40 -0700

    cleanup

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,22 @@
+#' @title Find error in delayed chain
+#' @description Searches through a network of delayed objects for the first object with state "error"
+#' @param delayed_object the object in which an error occured
+#' @export 
+find_delayed_error <- function(delayed_object){
+  if(delayed_object$state=='error'){
+    return(delayed_object)
+  } else{
+    errors <- sapply(delayed_object$delayed_dependencies,find_delayed_error)
+    
+    errors <- unlist(errors)
+    if(length(errors)>0){
+      return(errors[[1]])
+    } else {
+      return(NULL)
+    }
+  }
+}
+
 #' @rdname bundle_delayed
 #'
 #' @param ... Ignore (this is a convenience function)
