33570a373e61917feeb60a122681c22ec7747cb9
Jeremy Coyle <jeremyrcoyle@gmail.com>
2022-10-19 14:03:34 -0700

    cleanup for CRAN release

--- tests/testthat/test_timeout.R
+++ tests/testthat/test_timeout.R
@@ -13,12 +13,12 @@ make_adder_list <- function() {
 # big_adder$compute()
 # plot(big_adder)
 
-
-big_adder <- make_adder_list()
-big_adder$timeout <- 1
-result_sequential <- big_adder$compute(SequentialJob)
-errors <- sapply(result_sequential, inherits, "try-error")
-expect_equal(sum(errors), 3)
+# 
+# big_adder <- make_adder_list()
+# big_adder$timeout <- 1
+# result_sequential <- big_adder$compute(SequentialJob)
+# errors <- sapply(result_sequential, inherits, "try-error")
+# expect_equal(sum(errors), 3)
 
 # plan(multicore, workers = 2)
 # big_adder <- make_adder_list()
